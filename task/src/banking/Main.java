6545fbf83ab307de98ff0327454ace67e36af27e
Jeremy Grecu <jeremy.grecu@sap.com>
2021-08-25 16:15:49 +0100

    JBA - Simple Banking System - Stage 1/4: Card anatomy

--- task/src/banking/Main.java
+++ task/src/banking/Main.java
@@ -3,7 +3,7 @@ package banking;
 public class Main {
 
     public static void main(String[] args) {
-        TellerSystem atm = new TellerSystem();
+        UserInterface atm = new UserInterface();
         atm.processMain();
     }
 }
